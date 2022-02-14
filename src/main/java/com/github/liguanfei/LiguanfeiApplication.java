eec57049ca4ddd528545f52c09756c4b321ada8c
拽拽 <1050564479@qq.com>
2022-02-14 19:21:21 +0800

    init

---
+++ src/main/java/com/github/liguanfei/LiguanfeiApplication.java
@@ -0,0 +1,13 @@
+package com.github.liguanfei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiguanfeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiguanfeiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/liguanfei/util/StrUtil.java
@@ -0,0 +1,14 @@
+package com.github.liguanfei.util;
+
+/**
+ * @author cs
+ * @des
+ * @date 2022/2/14
+ */
+public class StrUtil {
+
+    public static void test(){
+        System.out.println("xxx");
+    }
+
+}
